793a19a4fdc50ec5ced970aefb98e6f98e307839
Axel Lundbäck <axel@ustwo.com>
2017-03-23 19:33:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# shakespeak-ios
---
+++ Shakespeak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shakespeak
+//
+//  Created by Axel Lundbäck on 23/03/2017.
+//  Copyright © 2017 shakespeak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
